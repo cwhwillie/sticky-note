9543845910c30c9c73c07f8d3eb01bd1cc58c47d
Willie_Chen <willie_chen@tp.office.openfind.com.tw>
2019-06-21 18:11:22 +0800

    fix selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sticky-note',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
