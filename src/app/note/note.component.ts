88fc6a81d2a3b56fe5c650caa5422c58f396e19f
yiyi <yiyi@yiyide-MacBook.local>
2019-06-26 16:18:23 +0800

    fix note position change issue when clicking note content.

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -14,6 +14,7 @@ import { NoteDataService } from '../note-data.service';
 export class NoteComponent implements OnInit, AfterViewInit {
   @Input() note: Note;
   @ViewChild('myNote') myNote: ElementRef;
+  @ViewChild('noteHeader') noteHeader: ElementRef;
   isCreate: boolean;
   isReadonly: boolean;
   title: string;
@@ -37,7 +38,7 @@ export class NoteComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const mouseDown = fromEvent(this.myNote.nativeElement, 'mousedown');
+    const mouseDown = fromEvent(this.noteHeader.nativeElement, 'mousedown');
     const mouseUp = fromEvent(document.body, 'mouseup');
     const mouseMove = fromEvent(document.body, 'mousemove');
 
